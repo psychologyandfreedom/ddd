4adbc8a6dfc4d9bac1659e464d5a6226f96e9235
sunny <matthewnorman.nv@gmail.com>
2022-08-11 11:42:43 -0700

    deleted empty projects pages

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css'
+
+
+export default function Footer () {
+    return (
+        <>
+            {/* footer */}
+                    <footer className={styles.footer}>
+                        <p>Matthew Norman<br />
+                        Email: jobs.matthew@protonmail.com<br />
+                        
+                        </p>
+                    </footer>
+        </>
+    )
+}
